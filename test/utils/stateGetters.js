8a9a5ad3c5ddf6eece7e9cd620384d5eed1f45ca
Greg Dardis <dardis.greg@gmail.com>
2018-05-07 12:18:19 -0700

    made constants for tests in stateGetters.js

--- test/utils/stateGetters.js
+++ test/utils/stateGetters.js
@@ -55,6 +55,27 @@ import {
   TITLE_MONTHLY_INCOME
 } from '../../src/constants'
 
+const AFTER_REPAIR_VALUE = 140000.25
+const AFTER_REPAIR_VALUE_STRING = '140000.25'
+const AMORTIZATION_PERIOD = 25
+const AMORTIZATION_PERIOD_STRING = '25'
+const ANNUAL_CONSTANT_EXPENSES_GROWTH = 3.25
+const ANNUAL_CONSTANT_EXPENSES_GROWTH_STRING = '3.25'
+const ANNUAL_INCOME_GROWTH = 2.25
+const ANNUAL_INCOME_GROWTH_STRING = '2.25'
+const ANNUAL_PROPERTY_VALUE_GROWTH = 1.25
+const ANNUAL_PROPERTY_VALUE_GROWTH_STRING = '1.25'
+const CLOSING_COSTS = 3000.25
+const CLOSING_COSTS_STRING = '3000.25'
+const DOWN_PAYMENT = 20000.25
+const DOWN_PAYMENT_STRING = '20000.25'
+const OTHER_INITIAL_COSTS = 250.25
+const OTHER_INITIAL_COSTS_STRING = '250.25'
+const PURCHASE_PRICE = 100000.25
+const PURCHASE_PRICE_STRING = '100000.25'
+const REPAIR_COSTS = 25000.25
+const REPAIR_COSTS_STRING = '25000.25'
+
 const DEFAULT_INPUT_CONTENT = {
   [TITLE_GENERAL_INFO]: {
     [INPUT_ID_NEIGHBORHOOD]: 'A Neighborhood',
@@ -67,15 +88,15 @@ const DEFAULT_INPUT_CONTENT = {
     [INPUT_ID_SQUARE_FEET]: '1200'
   },
   [TITLE_INITIAL_PURCHASE]: {
-    [INPUT_ID_PURCHASE_PRICE]: '100000.25',
-    [INPUT_ID_DOWN_PAYMENT]: '20000.25',
+    [INPUT_ID_PURCHASE_PRICE]: PURCHASE_PRICE_STRING,
+    [INPUT_ID_DOWN_PAYMENT]: DOWN_PAYMENT_STRING,
     [INPUT_ID_LOAN_AMOUNT]: '80000.25',
     [INPUT_ID_INTEREST_RATE]: '6.25',
-    [INPUT_ID_AMORTIZATION_PERIOD]: '25',
-    [INPUT_ID_AFTER_REPAIR_VALUE]: '140000.25',
-    [INPUT_ID_REPAIR_COSTS]: '20000.25',
-    [INPUT_ID_CLOSING_COSTS]: '3000.25',
-    [INPUT_ID_OTHER_INITIAL_COSTS]: '250.25'
+    [INPUT_ID_AMORTIZATION_PERIOD]: AMORTIZATION_PERIOD_STRING,
+    [INPUT_ID_AFTER_REPAIR_VALUE]: AFTER_REPAIR_VALUE_STRING,
+    [INPUT_ID_REPAIR_COSTS]: REPAIR_COSTS_STRING,
+    [INPUT_ID_CLOSING_COSTS]: CLOSING_COSTS_STRING,
+    [INPUT_ID_OTHER_INITIAL_COSTS]: OTHER_INITIAL_COSTS_STRING
   },
   [TITLE_MONTHLY_INCOME]: {
     [INPUT_ID_RENTAL_INCOME]: '1800.25',
@@ -97,9 +118,9 @@ const DEFAULT_INPUT_CONTENT = {
     [INPUT_ID_OTHER_EXPENSES]: '6.25'
   },
   [TITLE_FUTURE_PROJECTIONS]: {
-    [INPUT_ID_PROPERTY_VALUE_GROWTH]: '1.25',
-    [INPUT_ID_ANNUAL_INCOME_GROWTH]: '2.25',
-    [INPUT_ID_ANNUAL_CONSTANT_EXPENSES_GROWTH]: '3.25'
+    [INPUT_ID_PROPERTY_VALUE_GROWTH]: ANNUAL_PROPERTY_VALUE_GROWTH_STRING,
+    [INPUT_ID_ANNUAL_INCOME_GROWTH]: ANNUAL_INCOME_GROWTH_STRING,
+    [INPUT_ID_ANNUAL_CONSTANT_EXPENSES_GROWTH]: ANNUAL_CONSTANT_EXPENSES_GROWTH_STRING
   }
 }
 
@@ -108,70 +129,70 @@ describe('utils/stateGetters', () => {
     it('returns proper value', () => {
       expect(getAnnualConstantExpensesGrowth(DEFAULT_INPUT_CONTENT))
         .to
-        .equal(3.25)
+        .equal(ANNUAL_CONSTANT_EXPENSES_GROWTH)
     })
   })
   describe('getAfterRepairValue', () => {
     it('returns proper value', () => {
       expect(getAfterRepairValue(DEFAULT_INPUT_CONTENT))
         .to
-        .equal(140000.25)
+        .equal(AFTER_REPAIR_VALUE)
     })
   })
   describe('getAnnualPropertyValueGrowth', () => {
     it('returns proper value', () => {
       expect(getAnnualPropertyValueGrowth(DEFAULT_INPUT_CONTENT))
         .to
-        .equal(1.25)
+        .equal(ANNUAL_PROPERTY_VALUE_GROWTH)
     })
   })
   describe('getAnnualIncomeGrowth', () => {
     it('returns proper value', () => {
       expect(getAnnualIncomeGrowth(DEFAULT_INPUT_CONTENT))
         .to
-        .equal(2.25)
+        .equal(ANNUAL_INCOME_GROWTH)
     })
   })
   describe('getAmortizationPeriod', () => {
     it('returns proper value', () => {
       expect(getAmortizationPeriod(DEFAULT_INPUT_CONTENT))
         .to
-        .equal(25)
+        .equal(AMORTIZATION_PERIOD)
     })
   })
   describe('getDownPayment', () => {
     it('returns proper value', () => {
       expect(getDownPayment(DEFAULT_INPUT_CONTENT))
         .to
-        .equal(20000.25)
+        .equal(DOWN_PAYMENT)
     })
   })
   describe('getRepairCosts', () => {
     it('returns proper value', () => {
       expect(getRepairCosts(DEFAULT_INPUT_CONTENT))
         .to
-        .equal(20000.25)
+        .equal(REPAIR_COSTS)
     })
   })
   describe('getClosingCosts', () => {
     it('returns proper value', () => {
       expect(getClosingCosts(DEFAULT_INPUT_CONTENT))
         .to
-        .equal(3000.25)
+        .equal(CLOSING_COSTS)
     })
   })
   describe('getOtherInitialCosts', () => {
     it('returns proper value', () => {
       expect(getOtherInitialCosts(DEFAULT_INPUT_CONTENT))
         .to
-        .equal(250.25)
+        .equal(OTHER_INITIAL_COSTS)
     })
   })
   describe('getPurchasePrice', () => {
     it('returns proper value', () => {
       expect(getPurchasePrice(DEFAULT_INPUT_CONTENT))
         .to
-        .equal(100000.25)
+        .equal(PURCHASE_PRICE)
     })
   })
 })
