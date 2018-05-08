c9a0b7fcc59da9406661377cd538dd6d15c310ed
Greg Dardis <dardis.greg@gmail.com>
2018-05-08 08:41:26 -0700

    tested getYearsForResults

---
+++ test/utils/resultsUtils.js
@@ -0,0 +1,34 @@
+import { expect } from 'chai'
+
+import {
+  getYearsForResults
+} from '../../src/utils/resultsUtils'
+
+describe('utils/resultsUtils', () => {
+  describe('getYearsForResults', () => {
+    it('returns proper array when given a number not divisible by 5', () => {
+      expect(getYearsForResults(23))
+        .to
+        .deep
+        .equal([1, 5, 10, 15, 20, 24])
+    })
+    it('returns proper array when given a number divisible by 5', () => {
+      expect(getYearsForResults(25))
+        .to
+        .deep
+        .equal([1, 5, 10, 15, 20, 25, 26])
+    })
+    it('returns proper array when given 0', () => {
+      expect(getYearsForResults(0))
+        .to
+        .deep
+        .equal([1])
+    })
+    it('returns proper array when given the empty string', () => {
+      expect(getYearsForResults(''))
+        .to
+        .deep
+        .equal([1])
+    })
+  })
+})
