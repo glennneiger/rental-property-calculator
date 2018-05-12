006501fadd140e301aa69fceba90f8c667c0d494
Greg Dardis <dardis.greg@gmail.com>
2018-05-12 12:41:29 -0700

    made YearResult slightly more DRY

--- src/react/components/YearResult/YearResult.js
+++ src/react/components/YearResult/YearResult.js
@@ -16,21 +16,50 @@ const YearResult = ({
   result,
   year
 }) => {
+  const formatDisplayEntry = (entryTitle, entry, prefix = '', suffix = '') => (
+    <p>
+      {
+        `${ entryTitle }:
+        ${ prefix }${ entry.toFixed(NUMBER_PRECISION_DISPLAY) }${ suffix }`
+      }
+    </p>
+  )
   return (
     <div className='yearResult'>
       <h3>Year { year }</h3>
-      <p>Cash Flow: ${ result[RESULTS_CASH_FLOW]
-        .toFixed(NUMBER_PRECISION_DISPLAY) }</p>
-      <p>Cash on Cash Return: { result[RESULTS_CASH_ON_CASH_RETURN]
-        .toFixed(NUMBER_PRECISION_DISPLAY) }%</p>
-      <p>Property Value: ${ result[RESULTS_PROPERTY_VALUE]
-        .toFixed(NUMBER_PRECISION_DISPLAY) }</p>
-      <p>Equity: ${ result[RESULTS_EQUITY]
-        .toFixed(NUMBER_PRECISION_DISPLAY) }</p>
-      <p>Return on Equity: { result[RESULTS_RETURN_ON_EQUITY]
-        .toFixed(NUMBER_PRECISION_DISPLAY) }%</p>
-      <p>Return on Investment: { result[RESULTS_RETURN_ON_INVESTMENT]
-        .toFixed(NUMBER_PRECISION_DISPLAY) }%</p>
+      { formatDisplayEntry(
+        'Cash Flow',
+        result[RESULTS_CASH_FLOW],
+        '$'
+      ) }
+      { formatDisplayEntry(
+        'Cash on Cash Return',
+        result[RESULTS_CASH_ON_CASH_RETURN],
+        '',
+        '%'
+      ) }
+      { formatDisplayEntry(
+        'Property Value',
+        result[RESULTS_PROPERTY_VALUE],
+        '$'
+      ) }
+      { formatDisplayEntry(
+        'Equity',
+        result[RESULTS_EQUITY],
+        '$'
+      ) }
+      { formatDisplayEntry(
+        'Return on Equity',
+        result[RESULTS_RETURN_ON_EQUITY],
+        '',
+        '%'
+      ) }
+      { formatDisplayEntry(
+        'Return on Investment',
+        result[RESULTS_RETURN_ON_INVESTMENT],
+        '',
+        '%'
+      ) }
     </div>
   )
 }
