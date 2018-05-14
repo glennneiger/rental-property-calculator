76128750ea7611f7731e26146176d5696a53ab59
Greg Dardis <dardis.greg@gmail.com>
2018-05-14 11:20:37 -0700

    added mongoose

---
+++ src/server/schemas/index.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  passwordConf: {
+    type: String,
+    required: true
+  }
+})
+export const User = mongoose.model('User', userSchema)
