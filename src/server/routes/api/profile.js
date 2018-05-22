e35517b493b97ff80fe0dc32898a9da56f4d41e8
Greg Dardis <dardis.greg@gmail.com>
2018-05-22 11:00:44 -0700

    get current profile populates with user info. post route to create profile with custom handle

--- src/server/routes/api/profile.js
+++ src/server/routes/api/profile.js
@@ -4,6 +4,7 @@ import passport from 'passport'
 
 import Profile from '../../models/Profile'
 import User from '../../models/User'
+import { validateProfileInput } from '../../validation/profile'
 
 const router = express.Router()
 
@@ -23,8 +24,9 @@ router.get(
   '/',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    let errors = {}
+    const errors = {}
     Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'date'])
       .then(profile => {
         if (!profile) {
           errors.noProfile = 'No profile exists for this user'
@@ -33,6 +35,52 @@ router.get(
         return res.json(profile)
       })
       .catch(err => res.status(404).json(err))
+  }
+)
+
+// @route   POST api/profile
+// @desc    Create or edit user profile
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body)
+
+    if (!isValid) {
+      return res.status(400).json(errors)
+    }
+
+    const profileFields = {}
+    profileFields.user = req.user.id
+    if (req.body.handle) {
+      profileFields.handle = req.body.handle
+    }
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (profile) {
+          // Update if already exists
+          Profile.findOneAndUpdate(
+            { user: req.user.id },
+            { $set: profileFields },
+            { new: true }
+          ).then(updatedProfile => res.json(updatedProfile))
+        } else {
+          // Create profile for user
+          Profile.findOne({ handle: profileFields.handle })
+            .then(profileWithHandle => {
+              if (profileWithHandle) {
+                errors.handle = 'That handle is taken.'
+                return res.status(400).json(errors)
+              }
+
+              new Profile(profileFields).save().then(newProfile => {
+                return res.json(newProfile)
+              })
+            })
+        }
+      })
   }
 )
 
