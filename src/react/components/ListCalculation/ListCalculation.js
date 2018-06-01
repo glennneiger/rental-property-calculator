5422c4e920338406be91e8f84e2e872d4a8c0368
Greg Dardis <dardis.greg@gmail.com>
2018-06-01 08:36:02 -0700

    clicking on ListCalculation prints it's id to the console

--- src/react/components/ListCalculation/ListCalculation.js
+++ src/react/components/ListCalculation/ListCalculation.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import './listCalculation.css'
 
-const ListCalculation = ({
-  title
-}) => (
-  <div className='listCalculation'>
-    {title}
-  </div>
-)
+class ListCalculation extends Component {
+  constructor(props) {
+    super(props)
+  }
+  handleClick = () => {
+    console.log(this.props.id)
+  }
+  render() {
+    return (
+      <div className='listCalculation'
+        onClick={this.handleClick}>
+        {this.props.title}
+      </div>
+    )
+  }
+}
 
 ListCalculation.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired
 }
 
