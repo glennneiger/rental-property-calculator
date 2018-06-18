4622707a6fc59e3312241f8ab9d33819500e1866
Greg Dardis <dardis.greg@gmail.com>
2018-06-18 08:16:31 -0700

    made sidebar take up whole screen if less than 600 px

--- src/react/components/CalculatorPage/CalculatorPage.js
+++ src/react/components/CalculatorPage/CalculatorPage.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import Calculator from '../Calculator'
 import Sidebar from '../Sidebar'
 import css from './calculatorPage.css'
 
-const CalculatorPage = ({
-  sidebarVisible
-}) => {
-  return (
-    <div className={css.calculatorPage}>
-      {sidebarVisible
-        ? <Sidebar />
-        : null
-      }
-      <Calculator />
-    </div>
-  )
+class CalculatorPage extends Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount = () => {
+    window.addEventListener('resize', this.updateWidth)
+  }
+  componentWillUnmount = () => {
+    window.removeEventListener('resize', this.updateWidth)
+  }
+  componentWillMount = () => {
+    this.updateWidth()
+  }
+  updateWidth = () => {
+    this.setState({
+      width: window.innerWidth
+    })
+  }
+  render() {
+    return (
+      <div className={css.calculatorPage}>
+        {this.props.sidebarVisible
+          ? <Sidebar />
+          : null
+        }
+        {this.state.width < 600 && this.props.sidebarVisible
+          ? null
+          : <Calculator />
+        }
+      </div>
+    )
+  }
 }
 
 CalculatorPage.propTypes = {
