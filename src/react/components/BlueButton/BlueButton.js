5e3fa4d96e6b67e1a4f8839d9203d7d8b131c382
Greg Dardis <dardis.greg@gmail.com>
2018-10-11 13:38:25 -0700

    fixed BlueButton styling bug and wrote test for DisplayEntry

--- src/react/components/BlueButton/BlueButton.js
+++ src/react/components/BlueButton/BlueButton.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import './blueButton.css'
+import css from './blueButton.css'
 
 const BlueButton = props =>
-  <button className='blueButton' {...props}>
+  <button className={css.blueButton} {...props}>
     {props.children}
   </button>
 
