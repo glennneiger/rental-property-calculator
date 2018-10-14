5e57f979051116b2fc2b5cb56e1c1f0cacaa49fb
Greg Dardis <dardis.greg@gmail.com>
2018-10-14 11:14:44 -0700

    tested for BlueButtons and paragraphs in GuestSidebar

--- src/react/components/GuestSidebar/GuestSidebar.test.js
+++ src/react/components/GuestSidebar/GuestSidebar.test.js
@@ -14,4 +14,22 @@ describe('<GuestSidebar />', () => {
   test('container has proper className for styling', () => {
     expect(wrapper).to.have.className('guestSidebar');
   });
+
+  it('renders 2 BlueButtons', () => {
+    expect(wrapper.find('BlueButton')).to.have.length(2);
+  });
+
+  it('renders 2 paragraphs', () => {
+    expect(wrapper.find('p')).to.have.length(2);
+  });
+
+  it('has the Login button as the first BlueButton', () => {
+    expect(wrapper.find('BlueButton').at(0).prop('children'))
+      .to.equal('Login');
+  });
+
+  it('has the Register button as the second BlueButton', () => {
+    expect(wrapper.find('BlueButton').at(1).prop('children'))
+      .to.equal('Register');
+  });
 });
