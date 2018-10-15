a4612ca2c525c8b6f824ca4848bcc117685148d3
Greg Dardis <dardis.greg@gmail.com>
2018-10-15 14:49:35 -0700

    wrote calculator rendering tests for CalculatorPage

--- src/react/components/CalculatorPage/CalculatorPage.test.js
+++ src/react/components/CalculatorPage/CalculatorPage.test.js
@@ -6,6 +6,9 @@ import CalculatorPage from './CalculatorPage';
 import Sidebar from '../Sidebar';
 
 describe('<CalculatorPage />', () => {
+  const mockWideScreenWidth = 1920;
+  const mockNarrowScreenWidth = 500;
+
   it('has proper className for styling', () => {
     const wrapper = shallow(<CalculatorPage sidebarVisible={true} />);
 
@@ -14,9 +17,34 @@ describe('<CalculatorPage />', () => {
 
   it('renders Sidebar when sidebarVisible is true', () => {
     const wrapper = shallow(
-      <CalculatorPage screenWidth={700} sidebarVisible={true} />
+      <CalculatorPage screenWidth={mockWideScreenWidth} sidebarVisible={true} />
     );
 
     chaiExpect(wrapper.find(Sidebar)).to.have.length(1);
+  });
+
+  it('renders no Sidebar when sidebarVisible is false', () => {
+    const wrapper = shallow(
+      <CalculatorPage screenWidth={mockWideScreenWidth} sidebarVisible={false} />
+    );
+
+    chaiExpect(wrapper.find(Sidebar)).to.have.length(0);
+  });
+
+  it('renders Calculator when screenWidth is more than sidebar max', () => {
+    const wrapper = shallow(
+      <CalculatorPage screenWidth={mockWideScreenWidth} sidebarVisible={true} />
+    );
+
+    chaiExpect(wrapper.find('Calculator')).to.have.length(1);
+  });
+
+  it('does not render Calculator when screenWidth is less than' +
+    ' sidebar max and sidebar is showing', () => {
+    const wrapper = shallow(
+      <CalculatorPage screenWidth={mockNarrowScreenWidth} sidebarVisible={true} />
+    );
+
+    chaiExpect(wrapper.find('Calculator')).to.have.length(0);
   });
 });
