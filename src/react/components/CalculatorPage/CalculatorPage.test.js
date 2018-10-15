b23f87f8a0e0f9e458efa4d5102c7f9472980a15
Greg Dardis <dardis.greg@gmail.com>
2018-10-15 14:33:46 -0700

    tested for Sidebar rendering in CalculatorPage

--- src/react/components/CalculatorPage/CalculatorPage.test.js
+++ src/react/components/CalculatorPage/CalculatorPage.test.js
@@ -3,15 +3,20 @@ import { shallow } from 'enzyme';
 import { expect as chaiExpect } from 'chai';
 
 import CalculatorPage from './CalculatorPage';
+import Sidebar from '../Sidebar';
 
 describe('<CalculatorPage />', () => {
-  let wrapper;
+  it('has proper className for styling', () => {
+    const wrapper = shallow(<CalculatorPage sidebarVisible={true} />);
 
-  beforeAll(() => {
-    wrapper = shallow(<CalculatorPage />);
+    chaiExpect(wrapper).to.have.className('calculatorPage');
   });
 
-  it('has proper className for styling', () => {
-    chaiExpect(wrapper).to.have.className('calculatorPage');
+  it('renders Sidebar when sidebarVisible is true', () => {
+    const wrapper = shallow(
+      <CalculatorPage screenWidth={700} sidebarVisible={true} />
+    );
+
+    chaiExpect(wrapper.find(Sidebar)).to.have.length(1);
   });
 });
