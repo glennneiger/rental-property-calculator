fffc4a5199d7ae09bd61b91b3375a9718e77afc9
Greg Dardis <dardis.greg@gmail.com>
2018-10-17 13:58:12 -0700

    used mock data to assert correct number of CalculatorInputs were rendered

--- src/react/components/Calculator/Calculator.test.js
+++ src/react/components/Calculator/Calculator.test.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { expect as chaiExpect } from 'chai';
 import ShallowRenderer from 'react-test-renderer/shallow';
-import { inputSectionData } from './childProps';
+import * as childProps from './childProps';
+import CalculatorInput from '../CalculatorInput';
 
 import Calculator from './Calculator';
+
+const originalInputSectionData = childProps.inputSectionData;
 
 describe('<Calculator />', () => {
   it('has proper className for styling', () => {
@@ -22,10 +25,42 @@ describe('<Calculator />', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it('renders CalculatorInputSections', () => {
+  it('renders the correct number of CalculatorInputSections', () => {
     const wrapper = shallow(<Calculator />);
 
     chaiExpect(wrapper.find('CalculatorInputSection'))
-      .to.have.length(inputSectionData.length);
+      .to.have.length(childProps.inputSectionData.length);
+  });
+
+  it('renders correct number of CalculatorInputs', () => {
+    const mockTitle = 'General Info';
+    const mockInputProps = [
+      {
+        inputId: 1,
+        inputType: 'text',
+        label: 'Neighborhood',
+        textInputWidth: 200
+      },
+      {
+        inputId: 2,
+        inputType: 'text',
+        label: 'Street Address',
+        textInputWidth: 210
+      }
+    ];
+
+    childProps.inputSectionData = [
+      {
+        title: mockTitle,
+        childProps: mockInputProps
+      }
+    ];
+
+    const wrapper = shallow(<Calculator />);
+
+    chaiExpect(wrapper.find(CalculatorInput))
+      .to.have.length(mockInputProps.length);
+
+    childProps.inputSectionData = originalInputSectionData;
   });
 });
