d34ca851096ea5e396beded261e2d1cdad880175
Greg Dardis <dardis.greg@gmail.com>
2018-12-26 12:23:42 -0700

    Tested proper title is rendered in YearResult

--- src/react/components/YearResult/YearResult.test.js
+++ src/react/components/YearResult/YearResult.test.js
@@ -6,10 +6,19 @@ import YearResult from './YearResult';
 
 describe('<YearResult />', () => {
   let wrapper;
+  const mockYear = 2;
 
   it('has proper className for styling', () => {
-    wrapper = shallow(<YearResult result={{}} year={0} />);
+    wrapper = shallow(<YearResult result={{}} year={mockYear} />);
 
     expect(wrapper).to.have.className('yearResult');
   });
+  it('renders proper title including year', () => {
+    wrapper = shallow(<YearResult result={{}} year={mockYear} />);
+
+    expect(wrapper.find('h3').first().text()).to.equal(`Year ${mockYear}`);
+  });
 });
+// TODO: DisplayEntry content prop is undefined
+// Committed all changes, figure this out next.
+// Then write more tests
