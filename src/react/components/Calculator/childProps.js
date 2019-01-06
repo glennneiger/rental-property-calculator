55a78f9dbee684c8fef79fe142736c92e15f42d6
Greg Dardis <dardis.greg@gmail.com>
2019-01-06 13:43:09 -0800

    Refactored child props

--- src/react/components/Calculator/childProps.js
+++ src/react/components/Calculator/childProps.js
@@ -35,6 +35,41 @@ import {
   INPUT_ID_UNIT_NUMBER,
   INPUT_ID_VACANCY,
   INPUT_ID_WATER_AND_SEWER,
+  LABEL_AFTER_REPAIR_VALUE,
+  LABEL_AMORTIZATION_PERIOD,
+  LABEL_CAP_EX,
+  LABEL_CITY,
+  LABEL_CLOSING_COSTS,
+  LABEL_CONSTANT_EXPENSES_GROWTH,
+  LABEL_COUNTRY,
+  LABEL_DOWN_PAYMENT,
+  LABEL_ELECTRICITY,
+  LABEL_GARBAGE,
+  LABEL_HOA,
+  LABEL_INCOME_GROWTH,
+  LABEL_INSURANCE,
+  LABEL_INTEREST_RATE,
+  LABEL_LOAN_AMOUNT,
+  LABEL_MANAGEMENT,
+  LABEL_MORTGAGE,
+  LABEL_NEIGHBORHOOD,
+  LABEL_OTHER_EXPENSES,
+  LABEL_OTHER_INCOME,
+  LABEL_OTHER_INITIAL_COSTS,
+  LABEL_POSTAL_OR_ZIP_CODE,
+  LABEL_PRIVATE_MORTGAGE_INSURANCE,
+  LABEL_PROPERTY_TAX,
+  LABEL_PROPERTY_VALUE_GROWTH,
+  LABEL_PROVINCE_OR_STATE,
+  LABEL_PURCHASE_PRICE,
+  LABEL_RENTAL_INCOME,
+  LABEL_REPAIR_COSTS,
+  LABEL_REPAIRS_AND_MAINTENANCE,
+  LABEL_SQUARE_FEET,
+  LABEL_STREET_ADDRESS,
+  LABEL_UNIT_NUMBER,
+  LABEL_VACANCY,
+  LABEL_WATER_AND_SEWER,
   TITLE_FUTURE_PROJECTIONS,
   TITLE_GENERAL_INFO,
   TITLE_INITIAL_PURCHASE,
@@ -46,49 +81,49 @@ const generalInfoInputProps = [
   {
     inputId: INPUT_ID_NEIGHBORHOOD,
     inputType: 'text',
-    label: 'Neighborhood',
+    label: LABEL_NEIGHBORHOOD,
     textInputWidth: 200
   },
   {
     inputId: INPUT_ID_STREET_ADDRESS,
     inputType: 'text',
-    label: 'Street Address',
+    label: LABEL_STREET_ADDRESS,
     textInputWidth: 210
   },
   {
     inputId: INPUT_ID_UNIT_NUMBER,
     inputType: 'text',
-    label: 'Unit Number',
+    label: LABEL_UNIT_NUMBER,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_CITY,
     inputType: 'text',
-    label: 'City',
+    label: LABEL_CITY,
     textInputWidth: 200
   },
   {
     inputId: INPUT_ID_PROVINCE_OR_STATE,
     inputType: 'text',
-    label: 'Province/State',
+    label: LABEL_PROVINCE_OR_STATE,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_COUNTRY,
     inputType: 'text',
-    label: 'Country',
+    label: LABEL_COUNTRY,
     textInputWidth: 200
   },
   {
     inputId: INPUT_ID_POSTAL_OR_ZIP_CODE,
     inputType: 'text',
-    label: 'Postal/Zip Code',
+    label: LABEL_POSTAL_OR_ZIP_CODE,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_SQUARE_FEET,
     inputType: 'number',
-    label: 'Square Feet',
+    label: LABEL_SQUARE_FEET,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   }
 ];
@@ -97,55 +132,55 @@ const initialPurchaseInputProps = [
   {
     inputId: INPUT_ID_PURCHASE_PRICE,
     inputType: 'number',
-    label: 'Purchase Price',
+    label: LABEL_PURCHASE_PRICE,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_DOWN_PAYMENT,
     inputType: 'number',
-    label: 'Down Payment',
+    label: LABEL_DOWN_PAYMENT,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_LOAN_AMOUNT,
     inputType: 'number',
-    label: 'Loan Amount',
+    label: LABEL_LOAN_AMOUNT,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_INTEREST_RATE,
     inputType: 'number',
-    label: 'Interest Rate (%)',
+    label: LABEL_INTEREST_RATE,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_AMORTIZATION_PERIOD,
     inputType: 'number',
-    label: 'Amortization Period (years)',
+    label: LABEL_AMORTIZATION_PERIOD,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_AFTER_REPAIR_VALUE,
     inputType: 'number',
-    label: 'After Repair Value',
+    label: LABEL_AFTER_REPAIR_VALUE,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_REPAIR_COSTS,
     inputType: 'number',
-    label: 'Repair Costs',
+    label: LABEL_REPAIR_COSTS,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_CLOSING_COSTS,
     inputType: 'number',
-    label: 'Closing Costs',
+    label: LABEL_CLOSING_COSTS,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_OTHER_INITIAL_COSTS,
     inputType: 'number',
-    label: 'Other Initial Costs',
+    label: LABEL_OTHER_INITIAL_COSTS,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   }
 ];
@@ -155,13 +190,13 @@ export const incomeInputProps = [
   {
     inputId: INPUT_ID_RENTAL_INCOME,
     inputType: 'number',
-    label: 'Rental Income',
+    label: LABEL_RENTAL_INCOME,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_OTHER_INCOME,
     inputType: 'number',
-    label: 'Other Income',
+    label: LABEL_OTHER_INCOME,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   }
 ];
@@ -171,84 +206,84 @@ export const expensesInputProps = [
   {
     inputId: INPUT_ID_MORTGAGE,
     inputType: 'number',
-    label: 'Mortgage',
+    label: LABEL_MORTGAGE,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_ELECTRICITY,
     inputType: 'number',
-    label: 'Electricity',
+    label: LABEL_ELECTRICITY,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_WATER_AND_SEWER,
     inputType: 'number',
-    label: 'Water and Sewer',
+    label: LABEL_WATER_AND_SEWER,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_PRIVATE_MORTGAGE_INSURANCE,
     inputType: 'number',
-    label: 'Private Mortgage Insurance',
+    label: LABEL_PRIVATE_MORTGAGE_INSURANCE,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_GARBAGE,
     inputType: 'number',
-    label: 'Garbage',
+    label: LABEL_GARBAGE,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_HOA,
     inputType: 'number',
-    label: 'HOA Fees',
+    label: LABEL_HOA,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_INSURANCE,
     inputType: 'number',
-    label: 'Insurance',
+    label: LABEL_INSURANCE,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_PROPERTY_TAX,
     inputType: 'number',
-    label: 'Property Tax (%)',
+    label: LABEL_PROPERTY_TAX,
     percentOfPropertyValue: true,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_VACANCY,
     inputType: 'number',
-    label: 'Vacancy (%)',
+    label: LABEL_VACANCY,
     percentOfRent: true,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_REPAIRS_AND_MAINTENANCE,
     inputType: 'number',
-    label: 'Repairs & Maintenance (%)',
+    label: LABEL_REPAIRS_AND_MAINTENANCE,
     percentOfRent: true,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_CAP_EX,
     inputType: 'number',
-    label: 'Cap. Ex. (%)',
+    label: LABEL_CAP_EX,
     percentOfRent: true,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_MANAGEMENT,
     inputType: 'number',
-    label: 'Management (%)',
+    label: LABEL_MANAGEMENT,
     percentOfRent: true,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_OTHER_EXPENSES,
     inputType: 'number',
-    label: 'Other Expenses',
+    label: LABEL_OTHER_EXPENSES,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   }
 ];
@@ -257,19 +292,19 @@ export const futureProjectionsInputProps = [
   {
     inputId: INPUT_ID_PROPERTY_VALUE_GROWTH,
     inputType: 'number',
-    label: 'Annual Property Value Growth (%)',
+    label: LABEL_PROPERTY_VALUE_GROWTH,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_ANNUAL_INCOME_GROWTH,
     inputType: 'number',
-    label: 'Annual Income Growth (%)',
+    label: LABEL_INCOME_GROWTH,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   },
   {
     inputId: INPUT_ID_ANNUAL_CONSTANT_EXPENSES_GROWTH,
     inputType: 'number',
-    label: 'Annual Constant Expenses Growth (%)',
+    label: LABEL_CONSTANT_EXPENSES_GROWTH,
     textInputWidth: CALCULATOR_INPUT_WIDTH_DEFAULT
   }
 ];
