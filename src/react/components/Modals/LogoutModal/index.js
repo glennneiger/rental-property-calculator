7e06cc22d7681ed5665811599bc1843ea3ed0420
Greg <dardis.greg@gmail.com>
2019-01-27 12:59:04 -0800

    Cleared all calculator fields on logout

--- src/react/components/Modals/LogoutModal/index.js
+++ src/react/components/Modals/LogoutModal/index.js
@@ -3,8 +3,14 @@ import { connect } from 'react-redux';
 import LogoutModal from './LogoutModal';
 import { hideModal } from '../../../../actions/modal';
 import { logoutUser } from '../../../../actions/auth';
+import {
+  clearAllCalculatorFields
+} from '../../../../actions/calculatorFields';
 
 const mapDispatchToProps = dispatch => ({
+  clearAllCalculatorFields: () => {
+    dispatch(clearAllCalculatorFields());
+  },
   hideModal: () => {
     dispatch(hideModal());
   },
